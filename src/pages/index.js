34ce80da1d8321f95cde95d4adce50747f02a527
MCDAngelo <dangelo.maria@gmail.com>
2024-11-05 20:38:47 -0700

    Part 3 of tutorial, added image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 // Step 1: Import React
 import * as React from 'react';
 import Layout from "../components/layout";
+import { StaticImage } from 'gatsby-plugin-image'
+
 
 // Step 2: Define your component
 function IndexPage() {
@@ -8,6 +10,10 @@ function IndexPage() {
     <main>
       <Layout pageTitle="Home Page">
         <p>I'm making this by following the Gatsby Tutorial.</p>
+        <StaticImage
+        alt="Group of tasty looking fresh vegetables"
+        src="../images/veggies.jpg"
+       />
       </Layout>
     </main>
   )
